457a9d88cd4938fe11294c09f7d524f5c78f825c
Alber <alberdsp@gmail.com>
2023-12-21 00:58:39 +0100

    finalizados filtros y paginación completada

--- index.php
+++ index.php
@@ -3,12 +3,20 @@
 
 <?php
 
-
 // ABF 2023 
 
 
-// Inicializamos las variables
 
+// si se ha presionado limpiar, limpiamos formulario primero de todo
+
+if (isset($_POST['clear'])) {
+  header("Location: index.php");
+  exit;
+}
+
+
+
+// Inicializamos las variables
 
 
 $dni = '';
@@ -27,35 +35,154 @@ $pagina_seleccionada = 1;
 $pagina_actual = 1;
 
 
-
-if (isset($_POST['dni'])) {
-  $dni = $_POST['dni'];
-}
-if (isset($_POST['nombre'])) {
-  $nombre = $_POST['nombre'];
-}
-if (isset($_POST['localidad'])) {
-  $localidad = $_POST['localidad'];
-}
-if (isset($_POST['fechanacimiento'])) {
-  $fechanacimiento = $_POST['fechanacimiento'];
-}
-
-if (isset($_POST['lineas_pagina'])) {
-  $lineas_pagina = $_POST['lineas_pagina'];
-}
-//camputamos valor_inicial_limit si no viene, seria 0
-if (isset($_POST['valor_inicial_limit'])) {
-  $valor_inicial_limit = $_POST['valor_inicial_limit'];
-} else {
-  $valor_inicial_limit = 0;
-}
-
 // Conexión a la bd
 $servername = "localhost:3307";
 $username = "root";
 $password = "sauber";
 $dbname = "universidad";
+
+//  realizamos la conexión con el try y ejecutamos los filtros y la paginación si existen
+
+
+
+try {
+  $con = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+  $sqlfiltro = 'SELECT DNI, APELLIDO_1, APELLIDO_2, NOMBRE, DIRECCION, LOCALIDAD, PROVINCIA, FECHA_NACIMIENTO  FROM alumno WHERE 1 = 1';
+  $sqlcount = ' SELECT COUNT(*) as total FROM alumno WHERE 1 = 1';
+
+
+  // comenzamos a cargar los parámetros de la consulta
+  if (!empty($_POST['dni'])) {
+    $dni = $_POST['dni'];
+    $sqlfiltro .= ' AND dni = ?';
+    $sqlcount .= ' AND dni = ?';
+    $params[] = $dni;
+  }
+
+  if (!empty($_POST['nombre'])) {
+    $nombre = $_POST['nombre'];
+    $sqlfiltro .= ' AND nombre LIKE ?';
+    $sqlcount .= ' AND nombre LIKE ?';
+    $params[] = '%' . $nombre . '%';
+  }
+
+  if (!empty($_POST['localidad'])) {
+    $localidad = $_POST['localidad'];
+    $sqlfiltro .= ' AND localidad LIKE ?';
+    $sqlcount .= ' AND localidad LIKE ?';
+    $params[] = '%' . $localidad . '%';
+  }
+
+  if (!empty($_POST['fechanacimiento'])) {
+    $fechanacimiento = $_POST['fechanacimiento'];
+    $sqlfiltro .= ' AND fecha_nacimiento = ?';
+    $sqlcount .= ' AND fecha_nacimiento = ?';
+    $params[] = $fechanacimiento;
+  }
+
+
+  if (!empty($_POST['lineas_pagina'])) {
+    $lineas_pagina = $_POST['lineas_pagina'];
+  }
+
+  //camputamos valor_inicial_limit si no viene, seria 0
+
+  if (isset($_POST['valor_inicial_limit'])) {
+    $valor_inicial_limit = $_POST['valor_inicial_limit'];
+  } else {
+    $valor_inicial_limit = 0;
+  }
+
+
+  // Calcular el número total
+  $stmt = $con->prepare($sqlcount);
+  $stmt->execute($params);
+  $total_registros = $stmt->fetchColumn();
+
+  // Calcular el número de páginas 
+  $total_paginas = ceil($total_registros / $lineas_pagina);
+
+
+  if (isset($_POST['pagina_seleccionada'])) {
+    $pagina_actual = $_POST['pagina_seleccionada'];
+    $pagina_seleccionada = $_POST['pagina_seleccionada'];
+    $valor_inicial_limit =  $lineas_pagina * ($pagina_seleccionada - 1);
+    $valor_final_limit = $lineas_pagina;
+  }
+
+  //nos situamos en la primera
+  if (isset($_POST['primera'])) {
+    $valor_inicial_limit = 0;
+    $pagina_actual = 1;
+    $pagina_seleccionada = 1;
+    // retrocedemos una página
+
+  } elseif (isset($_POST['anterior'])) {
+    $valor_inicial_limit = max(0, $valor_inicial_limit - $lineas_pagina);
+    $pagina_actual--;
+    $pagina_seleccionada = $pagina_actual;
+
+    // comprobamos si estamos al final de la última página
+
+  } elseif (isset($_POST['siguiente'])) {
+    $pagina_actual++;
+    $pagina_seleccionada++;
+
+    if ($pagina_actual < $total_paginas) {
+
+
+      $valor_inicial_limit = min($total_registros - $lineas_pagina, $valor_inicial_limit + $lineas_pagina);
+    } else {
+
+      $pagina_actual = $total_paginas;
+      $pagina_seleccionada = $total_paginas;
+      $valor_inicial_limit =  $lineas_pagina * ($total_paginas - 1);
+      $valor_final_limit = $total_registros - $valor_inicial_limit;
+
+      $pagina_actual = $total_paginas;
+      $pagina_seleccionada = $pagina_actual;
+    }
+
+    //nos situamos en la última página
+
+  } elseif (isset($_POST['ultima'])) {
+    $valor_inicial_limit =  $lineas_pagina * ($total_paginas - 1);
+    $valor_final_limit = $total_registros - $valor_inicial_limit;
+
+    $pagina_actual = $total_paginas;
+    $pagina_seleccionada = $pagina_actual;
+  }
+
+  // controlamos que la paginación no se salga de los límites 
+
+  if ($pagina_seleccionada > $total_paginas) {
+    $pagina_seleccionada = $total_paginas;
+  }
+
+  if ($pagina_actual > $total_paginas) {
+    $pagina_actual = $total_paginas;
+  }
+
+  if ($pagina_seleccionada < 1) {
+    $pagina_seleccionada = 1;
+  }
+
+  if ($pagina_actual < 1) {
+    $pagina_actual = 1;
+  }
+
+  // establecemos limites y orden al filtro
+  $sqlfiltro .= " order by nombre ASC LIMIT $valor_inicial_limit , $lineas_pagina";
+
+  $stmt = $con->prepare($sqlfiltro);
+  $stmt->execute($params);
+  $resultados = $stmt->fetchAll(PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+  echo "Error: " . $e->getMessage();
+}
 
 ?>
 
@@ -88,11 +215,12 @@ $dbname = "universidad";
 
     <label for="fechanacimiento">F. Nacimiento:</label>
     <input type="date" id="fechanacimiento" name="fechanacimiento" value="<?php echo $fechanacimiento ?>">
-
-    <input type="submit" value="Buscar">
-    <input type="button" value="Limpiar" onclick="clearForm()">
-
     <input type="hidden" name="lineas_pagina" value="<?php echo htmlspecialchars($lineas_pagina); ?>" />
+    <input type="submit" value="Buscar">
+    <input type="submit" name="clear" value="Limpiar">
+
+
+
   </form>
 
   <table class="table">
@@ -114,123 +242,24 @@ $dbname = "universidad";
       <?php
 
 
-      //  realizamos la conexión con el try y ejecutamos los filtros y la paginación si existen
 
-      try {
-        $con = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-        $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-
-        $sqlfiltro = 'SELECT DNI, APELLIDO_1, APELLIDO_2, NOMBRE, DIRECCION, LOCALIDAD, PROVINCIA, FECHA_NACIMIENTO  FROM alumno WHERE 1 = 1';
-        $sqlcount = ' SELECT COUNT(*) as total FROM alumno WHERE 1 = 1';
-        // comenzamos a cargar los parámetros de la consulta
-        if (!empty($_POST['dni'])) {
-          $dni = $_POST['dni'];
-          $sqlfiltro .= ' AND dni = ?';
-          $sqlcount .= ' AND dni = ?';
-          $params[] = $dni;
+      if ($resultados) {
+        foreach ($resultados as $lista) {
+          echo "<tr class='celda'>";
+          echo "<td class='celda'>" . htmlspecialchars($lista['DNI']) . "</td>";
+          echo "<td class='celda'>" . htmlspecialchars($lista['NOMBRE']) . "</td>";
+          echo "<td class='celda'>" . htmlspecialchars($lista['APELLIDO_1']) . "</td>";
+          echo "<td class='celda'>" . htmlspecialchars($lista['APELLIDO_2']) . "</td>";
+          echo "<td class='celda'>" . htmlspecialchars($lista['DIRECCION']) . "</td>";
+          echo "<td class='celda'>" . htmlspecialchars($lista['LOCALIDAD']) . "</td>";
+          echo "<td class='celda'>" . htmlspecialchars($lista['PROVINCIA']) . "</td>";
+          echo "<td class='celda'>" . htmlspecialchars($lista['FECHA_NACIMIENTO']) . "</td>";
+          echo "</tr>";
         }
-
-        if (!empty($_POST['nombre'])) {
-          $nombre = $_POST['nombre'];
-          $sqlfiltro .= ' AND nombre LIKE ?';
-          $sqlcount .= ' AND nombre LIKE ?';
-          $params[] = '%' . $nombre . '%';
-        }
-
-        if (!empty($_POST['localidad'])) {
-          $localidad = $_POST['localidad'];
-          $sqlfiltro .= ' AND localidad LIKE ?';
-          $sqlcount .= ' AND localidad LIKE ?';
-          $params[] = '%' . $localidad . '%';
-        }
-
-        if (!empty($_POST['fechanacimiento'])) {
-          $fechanacimiento = $_POST['fechanacimiento'];
-          $sqlfiltro .= ' AND fecha_nacimiento = ?';
-          $sqlcount .= ' AND fecha_nacimiento = ?';
-          $params[] = $fechanacimiento;
-        }
-
-
-        if (!empty($_POST['lineas_pagina'])) {
-          $lineas_pagina = $_POST['lineas_pagina'];
-        }
-
-
-
-        // Calcular el número total
-        $stmt = $con->prepare($sqlcount);
-        $stmt->execute($params);
-        $total_registros = $stmt->fetchColumn();
-
-        $total_paginas = ceil($total_registros / $lineas_pagina);
-
-        // Calcular el número de páginas 
-        $total_paginas = ceil($total_registros / $lineas_pagina);
-
-        if (isset($_POST['primera'])) {
-          $valor_inicial_limit = 0;
-          $pagina_actual = 1;
-        } elseif (isset($_POST['anterior'])) {
-          $valor_inicial_limit = max(0, $valor_inicial_limit - $lineas_pagina);
-          $pagina_actual--;
-          $pagina_seleccionada = $pagina_actual;
-        } elseif (isset($_POST['siguiente'])) {
-          $pagina_actual++;
-          if ($pagina_actual != $total_paginas) {
-            $valor_inicial_limit = min($total_registros - $lineas_pagina, $valor_inicial_limit + $lineas_pagina);
-          } else {
-            $valor_inicial_limit =  $lineas_pagina * ($total_paginas - 1);
-            $lineas_pagina = $total_registros - $valor_inicial_limit;
-          }
-          $pagina_seleccionada = $pagina_actual;
-          echo "valor del limite incial " . $valor_inicial_limit;
-          echo "valor del limite final " . $lineas_pagina;
-        } elseif (isset($_POST['ultima'])) {
-          $valor_inicial_limit =  $lineas_pagina * ($total_paginas - 1);
-          $lineas_pagina = $total_registros - $valor_inicial_limit;
-          echo "valor del limite incial " . $valor_inicial_limit;
-          echo "valor del limite final " . $lineas_pagina;
-          echo "valor numero de páginas " . $total_paginas;
-          $pagina_actual = $total_paginas;
-          $pagina_seleccionada = $pagina_actual;
-          // TODO  ARREGLAR EL SELECTOR DE PÁGINA
-
-        } elseif (isset($_POST['pagina_seleccionada'])) {
-          $pagina_actual = $_POST['pagina_seleccionada'];
-          $pagina_seleccionada = $_POST['pagina_seleccionada'];
-          $valor_inicial_limit =  $lineas_pagina * $pagina_actual;
-          $valor_final_limit = $lineas_pagina;
-        }
-
-
-
-        $sqlfiltro .= " order by nombre ASC LIMIT $valor_inicial_limit , $lineas_pagina";
-
-        $stmt = $con->prepare($sqlfiltro);
-        $stmt->execute($params);
-        $resultados = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-        if ($resultados) {
-          foreach ($resultados as $lista) {
-            echo "<tr class='celda'>";
-            echo "<td class='celda'>" . htmlspecialchars($lista['DNI']) . "</td>";
-            echo "<td class='celda'>" . htmlspecialchars($lista['NOMBRE']) . "</td>";
-            echo "<td class='celda'>" . htmlspecialchars($lista['APELLIDO_1']) . "</td>";
-            echo "<td class='celda'>" . htmlspecialchars($lista['APELLIDO_2']) . "</td>";
-            echo "<td class='celda'>" . htmlspecialchars($lista['DIRECCION']) . "</td>";
-            echo "<td class='celda'>" . htmlspecialchars($lista['LOCALIDAD']) . "</td>";
-            echo "<td class='celda'>" . htmlspecialchars($lista['PROVINCIA']) . "</td>";
-            echo "<td class='celda'>" . htmlspecialchars($lista['FECHA_NACIMIENTO']) . "</td>";
-            echo "</tr>";
-          }
-        } else {
-          echo "<tr><td colspan='8'>No se han encontrado registros.</td></tr>";
-        }
-      } catch (PDOException $e) {
-        echo "Error: " . $e->getMessage();
+      } else {
+        echo "<tr><td colspan='8'>No se han encontrado registros.</td></tr>";
       }
+
       ?>
     </tbody>
   </table>
@@ -249,9 +278,6 @@ $dbname = "universidad";
     <input type="hidden" name="valor_inicial_limit" value="<?php echo htmlspecialchars($valor_inicial_limit); ?>" />
     <input type="hidden" name="pagina_actual" value="<?php echo htmlspecialchars($pagina_actual); ?>" />
     <input type="hidden" name="lineas_pagina" value="<?php echo htmlspecialchars($lineas_pagina); ?>" />
-
-
-
     <input type="submit" name="primera" value=" << " />
     <input type="submit" name="anterior" value=" < " />
 
@@ -260,11 +286,6 @@ $dbname = "universidad";
 
     <input type="text" name="pagina_seleccionada" value="<?php echo $pagina_seleccionada; ?>" />
     <input type="submit" value="ir a página">
-
-
-
-
-
     <input type="submit" name="siguiente" value=" > " />
     <input type="submit" name="ultima" value=" >> " />
 
